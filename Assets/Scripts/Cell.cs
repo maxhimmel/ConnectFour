6293b27e528c74b088ed2f571bc837fa0b7a91fb
Max Himmel <himmelmax@gmail.com>
2021-02-13 10:31:54 -0800

    + Created basic grid creation system

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ConnectFour.Gameplay
+{
+	public class Cell : MonoBehaviour
+	{
+		[SerializeField] private Image m_fill = default;
+
+		private LayoutElement m_layout = null;
+
+		private void OnEnable()
+		{
+			Empty();
+		}
+
+		public void Empty()
+		{
+			m_fill.enabled = false;
+		}
+
+		public void Fill()
+		{
+			m_fill.enabled = true;
+		}
+
+		public void SetSize( float newSize )
+		{
+			m_layout.preferredHeight = m_layout.preferredWidth = newSize;
+		}
+
+		private void Awake()
+		{
+			m_layout = GetComponent<LayoutElement>();
+		}
+	}
+}
---
+++ Assets/Scripts/Column.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ConnectFour.Gameplay
+{
+	public class Column : MonoBehaviour
+	{
+		private VerticalLayoutGroup m_layout = null;
+
+		private void Awake()
+		{
+			m_layout = GetComponent<VerticalLayoutGroup>();
+		}
+	}
+}
