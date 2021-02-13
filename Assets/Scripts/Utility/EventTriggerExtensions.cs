4134e85fa2ed95a280ff74ef1d3ee0ea0e658b8b
Max Himmel <himmelmax@gmail.com>
2021-02-13 10:58:22 -0800

    + Created utility class for event triggers (adding events is easier/cleaner)

---
+++ Assets/Scripts/Utility/EventTriggerExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace ConnectFour.Utility
+{
+	public static class EventTriggerExtensions
+	{
+		public static void AddEvent( this EventTrigger trigger, EventTriggerType type, UnityAction<BaseEventData> action )
+		{
+			if ( !trigger.TryGetEventEntry( type, out EventTrigger.Entry eventEntry ) )
+			{
+				trigger.triggers.Add( eventEntry );
+			}
+
+			eventEntry.callback.AddListener( action );
+		}
+
+		public static bool TryGetEventEntry( this EventTrigger trigger, EventTriggerType type, out EventTrigger.Entry entry )
+		{
+			entry = trigger.triggers.Find( e => e.eventID == type );
+			if ( entry != null ) { return true; }
+
+			entry = new EventTrigger.Entry();
+			entry.eventID = type;
+
+			return false;
+		}
+	}
+}
