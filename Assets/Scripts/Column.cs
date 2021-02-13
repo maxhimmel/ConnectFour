50425ecbdb3c6ac2724e108ea05adba0d418fd2b
Max Himmel <himmelmax@gmail.com>
2021-02-13 10:59:40 -0800

    + Added event trigger to column prefab + Registering click/pointer-enter events to column (for when we wanna place a piece later)

--- Assets/Scripts/Column.cs
+++ Assets/Scripts/Column.cs
@@ -2,16 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 namespace ConnectFour.Gameplay
 {
+	using Utility;
+
 	public class Column : MonoBehaviour
 	{
+		public int CurrentFillCount { get; private set; }
+
+		private int m_maxFill = -1;
+
 		private VerticalLayoutGroup m_layout = null;
+		private EventTrigger m_eventTrigger = null;
+
+		public void SetMaxFillCount( int maxFill )
+		{
+			m_maxFill = maxFill;
+		}
+
+		private void Start()
+		{
+			m_eventTrigger.AddEvent( EventTriggerType.PointerClick, OnClicked );
+			m_eventTrigger.AddEvent( EventTriggerType.PointerEnter, OnPointerEntered );
+		}
+
+		private void OnClicked( BaseEventData data )
+		{
+			//Debug.Log( $"[{name}] | Clicked!" );
+		}
+
+		private void OnPointerEntered( BaseEventData data )
+		{
+			//Debug.Log( $"[{name}] | Pointer entered!" );
+		}
 
 		private void Awake()
 		{
 			m_layout = GetComponent<VerticalLayoutGroup>();
+			m_eventTrigger = GetComponent<EventTrigger>();
 		}
 	}
 }
