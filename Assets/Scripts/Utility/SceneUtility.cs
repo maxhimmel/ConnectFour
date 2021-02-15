899d6ee91619cb9627165abc67cb62ef6082335c
Max Himmel <himmelmax@gmail.com>
2021-02-15 08:40:41 -0800

    + Added utility for loading next scene based on build indices + Added main menu and gameplay to build settings

---
+++ Assets/Scripts/Utility/SceneUtility.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ConnectFour.Utility
+{
+	public static class SceneUtility
+	{
+		public static void LoadNextScene()
+		{
+			Scene current = SceneManager.GetActiveScene();
+
+			int nextLevel = (current.buildIndex + 1) % SceneManager.sceneCountInBuildSettings;
+			SceneManager.LoadScene( nextLevel, LoadSceneMode.Single );
+		}
+	}
+}
