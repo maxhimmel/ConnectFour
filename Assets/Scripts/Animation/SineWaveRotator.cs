43a64e2cb8cfe51617ad4f5b269f2aca2ee83ae6
Max Himmel <himmelmax@gmail.com>
2021-02-15 13:40:56 -0800

    + Added pivot offset for rotator

--- Assets/Scripts/Animation/SineWaveRotator.cs
+++ Assets/Scripts/Animation/SineWaveRotator.cs
@@ -9,17 +9,23 @@ namespace ConnectFour.Animation
 	public class SineWaveRotator : MonoBehaviour
 	{
 		public bool IsPlaying { get { return m_rotateRoutine != null; } }
-
+		
 		[SerializeField] private float m_maxAngle = 20;
 		[SerializeField] private Vector3 m_axis = Vector3.forward;
 		[SerializeField] private WaveDatum m_sineWave = new WaveDatum( 1, 1, 0 );
 
+		[Space]
+		[SerializeField] private Transform m_rotationPivot = default;
+
 		private Coroutine m_rotateRoutine = null;
+		private Vector3 m_initialPivot;
 
 		[ContextMenu( "Play!" )]
 		public void Play()
 		{
 			Stop();
+
+			ResetInitialPivotPoint();
 			m_rotateRoutine = StartCoroutine( UpdateWave() );
 		}
 
@@ -33,17 +39,22 @@ namespace ConnectFour.Animation
 			}
 		}
 
+		public void ResetInitialPivotPoint()
+		{
+			m_initialPivot = transform.position;
+		}
+
 		private IEnumerator UpdateWave()
 		{
 			float timer = 0;
 			while ( enabled )
 			{
 				timer += Time.deltaTime;
-
 				float sine = m_sineWave.GetSinWave( timer );
-
 				Vector3 newEuler = m_axis * m_maxAngle * sine;
-				transform.eulerAngles = newEuler;
+
+				transform.position = RotatePointAroundPivot( m_initialPivot, GetPivot(), newEuler  );
+				transform.rotation = Quaternion.Euler( newEuler );
 
 				yield return null;
 			}
@@ -51,9 +62,31 @@ namespace ConnectFour.Animation
 			m_rotateRoutine = null;
 		}
 
+		private Vector3 RotatePointAroundPivot( Vector3 point, Vector3 pivot, Vector3 eulerAngles )
+		{
+			return Quaternion.Euler( eulerAngles ) * (point - pivot) + pivot;
+		}
+
+		private Vector3 GetPivot()
+		{
+			return m_rotationPivot != null
+				? m_rotationPivot.position
+				: transform.position;
+		}
+		
+		public void SetAmplitude( float amplitude )
+		{
+			m_sineWave.Amplitude = amplitude;
+		}
+
 		private void OnDisable()
 		{
 			Stop();
+		}
+
+		private void Start()
+		{
+			ResetInitialPivotPoint();
 		}
 	}
 }
