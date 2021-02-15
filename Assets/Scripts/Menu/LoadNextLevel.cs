d1c1f79c82acb8a9296b3bbfb3513fd6d9dd49c5
Max Himmel <himmelmax@gmail.com>
2021-02-15 08:41:01 -0800

    + Created load next level and quit game buttons for main menu

---
+++ Assets/Scripts/Menu/LoadNextLevel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ConnectFour.Ui
+{
+	using Utility;
+
+	[RequireComponent( typeof( Button ) )]
+	public class LoadNextLevel : MonoBehaviour
+	{
+		private Button m_button = null;
+
+		private void Start()
+		{
+			m_button.onClick.AddListener( SceneUtility.LoadNextScene );
+		}
+
+		private void Awake()
+		{
+			m_button = GetComponent<Button>();
+		}
+	}
+}
---
+++ Assets/Scripts/Menu/QuitGame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ConnectFour.Ui
+{
+	[RequireComponent( typeof( Button ) )]
+	public class QuitGame : MonoBehaviour
+	{
+		private Button m_button = null;
+
+		private void Start()
+		{
+			m_button.onClick.AddListener( Quit );
+		}
+
+		private void Quit()
+		{
+#if UNITY_EDITOR
+			UnityEditor.EditorApplication.ExitPlaymode();
+#else
+			Application.Quit();
+#endif
+		}
+
+		private void Awake()
+		{
+			m_button = GetComponent<Button>();
+		}
+	}
+}
