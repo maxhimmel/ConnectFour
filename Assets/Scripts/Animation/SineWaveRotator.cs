40ee2a6cf52a983ea7e16fb509daaf2a1f4f607a
Max Himmel <himmelmax@gmail.com>
2021-02-15 10:54:05 -0800

    + Created rotating anim for sine waves! JUICE

---
+++ Assets/Scripts/Animation/SineWaveRotator.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ConnectFour.Animation
+{
+	using Utility;
+
+	public class SineWaveRotator : MonoBehaviour
+	{
+		public bool IsPlaying { get { return m_rotateRoutine != null; } }
+
+		[SerializeField] private float m_maxAngle = 20;
+		[SerializeField] private Vector3 m_axis = Vector3.forward;
+		[SerializeField] private WaveDatum m_sineWave = new WaveDatum( 1, 1, 0 );
+
+		private Coroutine m_rotateRoutine = null;
+
+		[ContextMenu( "Play!" )]
+		public void Play()
+		{
+			Stop();
+			m_rotateRoutine = StartCoroutine( UpdateWave() );
+		}
+
+		[ContextMenu( "Stop, please" )]
+		public void Stop()
+		{
+			if ( IsPlaying )
+			{
+				StopCoroutine( m_rotateRoutine );
+				m_rotateRoutine = null;
+			}
+		}
+
+		private IEnumerator UpdateWave()
+		{
+			float timer = 0;
+			while ( enabled )
+			{
+				timer += Time.deltaTime;
+
+				float sine = m_sineWave.GetSinWave( timer );
+
+				Vector3 newEuler = m_axis * m_maxAngle * sine;
+				transform.eulerAngles = newEuler;
+
+				yield return null;
+			}
+
+			m_rotateRoutine = null;
+		}
+
+		private void OnDisable()
+		{
+			Stop();
+		}
+	}
+}
---
+++ Assets/Scripts/Utility/WaveDatum.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ConnectFour.Utility
+{
+	[System.Serializable]
+	public struct WaveDatum
+	{
+		public float Frequency;
+		public float Amplitude;
+		public float Phase;
+		
+		public WaveDatum( float frequency, float amplitude, float phase )
+		{
+			Frequency = frequency;
+			Amplitude = amplitude;
+			Phase = phase;
+		}
+
+		public float GetSinWave( float time )
+		{
+			return Amplitude * Mathf.Sin( Frequency * (time + Phase) );
+		}
+
+		public float GetCosWave( float time )
+		{
+			return Amplitude * Mathf.Cos( Frequency * (time + Phase) );
+		}
+	}
+}
