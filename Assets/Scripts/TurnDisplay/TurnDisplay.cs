c8f8b487a21078da692183067873277b1f5b5308
Max Himmel <himmelmax@gmail.com>
2021-02-14 21:44:08 -0800

    + Reorganized a bunch of scripts + Separated turn display logic into element and manager

---
+++ Assets/Scripts/TurnDisplay/TurnDisplay.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ConnectFour.Gameplay
+{
+	public class TurnDisplay : MonoBehaviour
+	{
+		private GameManager Game { get { return GameManager.Instance; } }
+		
+		[SerializeField] private Image m_display = default;
+
+		[Space]
+		[SerializeField] private float m_rotateDuration = 0.7f;
+		[SerializeField] private AnimationCurve m_rotateCurve = AnimationCurve.EaseInOut( 0, 0, 1, 1 );
+
+		private Coroutine m_showHideRoutine = null;
+
+		public void SetColor( int player )
+		{
+			m_display.color = Game.GetPlayerColor( player );
+		}
+
+		public void Show()
+		{
+			Stop();
+			m_showHideRoutine = StartCoroutine( RotateForSeconds( m_rotateDuration, 0 ) );
+		}
+
+		public void Hide()
+		{
+			Stop();
+			m_showHideRoutine = StartCoroutine( RotateForSeconds( m_rotateDuration, 180 ) );
+		}
+
+		private void Stop()
+		{
+			if ( m_showHideRoutine != null )
+			{
+				StopCoroutine( m_showHideRoutine );
+				m_showHideRoutine = null;
+			}
+		}
+
+		private IEnumerator RotateForSeconds( float duration, float targetAngle )
+		{
+			float startAngle = transform.eulerAngles.z;
+
+			float timer = 0;
+			while ( timer < 1 )
+			{
+				timer += Time.deltaTime / duration;
+				timer = Mathf.Clamp01( timer );
+
+				float sample = m_rotateCurve.Evaluate( timer );
+				float newAngle = Mathf.LerpUnclamped( startAngle, targetAngle, sample );
+
+				transform.eulerAngles = new Vector3()
+				{
+					x = transform.eulerAngles.x,
+					y = transform.eulerAngles.y,
+					z = newAngle
+				};
+
+				yield return null;
+			}
+
+			m_showHideRoutine = null;
+		}
+
+		private void OnDisable()
+		{
+			Stop();
+		}
+	}
+}
---
+++ Assets/Scripts/TurnDisplay/TurnDisplayManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ConnectFour.Gameplay
+{
+	public class TurnDisplayManager : MonoBehaviour
+	{
+		private GameManager Game { get { return GameManager.Instance; } }
+
+		private TurnDisplay[] m_displays = null;
+
+		private void Start()
+		{
+			Game.OnPlayerTurnChangedEvent += OnPlayerTurnChanged;
+
+			for ( int pdx = 0; pdx < m_displays.Length; ++pdx )
+			{
+				TurnDisplay display = m_displays[pdx];
+				display.SetColor( pdx );
+			}
+		}
+
+		private void OnPlayerTurnChanged( int player )
+		{
+			TurnDisplay currentDisplay = m_displays[player];
+			currentDisplay.Show();
+
+			int prevPlayer = (player + 1) % GameRules.k_playerCount;
+			TurnDisplay prevDisplay = m_displays[prevPlayer];
+			prevDisplay.Hide();
+		}
+
+		private void Awake()
+		{
+			m_displays = GetComponentsInChildren<TurnDisplay>();
+		}
+	}
+}
